68886d0e23f97acb449e99b48577dc3089b10010
eripratama1 <eripratama90@gmail.com>
2024-03-07 09:19:26 +0800

    install requiremnet package

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password')
+        ]);
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import 'flowbite';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
