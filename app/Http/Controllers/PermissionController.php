e7700b11dfb86e8dbdc64b22b539afece965f232
eripratama1 <eripratama90@gmail.com>
2024-03-08 14:06:55 +0800

    function create & read data permission

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('permission.index',[
+            'permissions' => Permission::latest()->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('permission.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|unique:permissions,name'
+        ]);
+
+        $permissions = new Permission;
+        $permissions->name = $request->input('name');
+        $permissions->save();
+
+        return to_route('manage-permission.index')->with('info','Permission berhasil ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('manage-permission.index')" :active="request()->routeIs('manage-permission*')">
+                        {{ __('Manage Permission') }}
+                    </x-nav-link>
                 </div>
             </div>
 
